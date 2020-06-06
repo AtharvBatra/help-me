355fe57d81b9d7c174b8aa864877de1eb09df311
AtharvBatra <atharv.batra@outlook.com>
2020-06-06 15:33:27 +0530

    defined the new classes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world, body;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1366,657);
+  engine = Engine.create();
+    world = engine.world;
+  ground = new Ground(680,height,1399,20);
+  ground1 = new Ground(390, 470, 250, 10);
+  ground2 = new Ground(866, 313, 250, 10);
+  stone = new Stone(33, 26, 30, 20);
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(255, 255, 255);
+  Engine.update(engine);
+  /*
+  console.log(mouseX);
+  console.log(mouseY);
+  */
+  
+  ground.display();
+  ground1.display();
+  ground2.display();
+  stone.display();
+  
+  
 }
